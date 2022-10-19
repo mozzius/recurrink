39e4045b4ea40c0c3cbe353a595f516ca98329c9
Samuel Newman <mozzius@protonmail.com>
2022-10-19 12:39:27 +0100

    set production url for API

--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -5,20 +5,8 @@ import type { AppRouter } from "@acme/api";
  */
 export const trpc = createTRPCReact<AppRouter>();
 
-/**
- * Extend this function when going to production by
- * setting the baseUrl to your production API URL.
- */
-import Constants from "expo-constants";
 const getBaseUrl = () => {
-  /**
-   * Gets the IP address of your host-machine. If it cannot automatically find it,
-   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
-   * you don't have anything else running on it, or you'd have to change it.
-   */
-  const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
-  if (!localhost) throw new Error("failed to get localhost, configure it manually");
-  return `http://${localhost}:3000`;
+  return "https://recurr.ink";
 };
 
 /**
@@ -27,7 +15,7 @@ const getBaseUrl = () => {
  */
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from '@trpc/client';
+import { httpBatchLink } from "@trpc/client";
 import { transformer } from "@acme/api/transformer";
 
 export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
@@ -42,7 +30,7 @@ export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
-    }),
+    })
   );
 
   return (
